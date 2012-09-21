94bc15d7f1860cacdfa9a423384f6dd9dd0b47e9
Alexander Savinoff <hinstance@yandex.ru>
2012-09-21 18:44:55 +0400

    Initial commit

---
+++ Copy-File.ps1
@@ -0,0 +1,20 @@
+﻿function Copy-File {
+    param([string]$from, [string]$to)
+    $ffile = [io.file]::OpenRead($from)
+    $tofile = [io.file]::OpenWrite($to)
+    Write-Progress -Activity "Copying file" -status "$from -> $to" -PercentComplete 0
+	
+    [byte[]]$buff = new-object byte[] 1048576
+    [int]$total = [int]$count = 0
+    do {
+        $count = $ffile.Read($buff, 0, $buff.Length)
+        $tofile.Write($buff, 0, $count)
+        $total += $count
+        if ($total % 1mb -eq 0) {
+            Write-Progress -Activity "Copying file" -status "$from -> $to" `
+               -PercentComplete ([int]($total/$ffile.Length* 100))
+        }
+    } while ($count -gt 0)
+    $ffile.Close()
+    $tofile.Close()
+}
---
+++ Deploy-File.ps1
@@ -0,0 +1,24 @@
+﻿. .\Copy-File.ps1
+
+function Deploy-FileTemp {
+	param([string]$source_file_path, [string]$destination_file_path)
+	$source_file_exists = Test-Path $source_file_path
+	$destination_file_exists = Test-Path $destination_file_path
+	if ($source_file_exists)
+	{
+		$temp_file_path = [System.IO.Path]::GetTempFileName()
+		Write-Host "Copying '$source_file_name' to '$temp_file_path'"
+		Copy-File $source_file_path $temp_file_path
+		if ($destination_file_exists)
+		{
+			Write-Host "Removing '$destination_file_path'"
+			Remove-Item $destination_file_path
+		}
+		Write-Host "Copying '$temp_file_path' to '$destination_file_path'"
+		Copy-File $temp_file_path $destination_file_path
+	}
+	else
+	{
+		Write-Error "Source file '$source_file_path' does not exist"
+	}
+}
