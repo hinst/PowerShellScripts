79a1233a7666965d8eb598b0b1bf431c126b30d2
Alexander Savinoff <hinstance@yandex.ru>
2012-10-15 23:29:38 +0400

    up

--- Deploy-File.ps1
+++ Deploy-File.ps1
@@ -5,7 +5,7 @@ function Deploy-FileTemp {
 	if (Test-Path $source_file_path)
 	{
 		$temp_file_path = [System.IO.Path]::GetTempFileName()
-		Write-Host "Copying '$source_file_name' to '$temp_file_path'"
+		Write-Host "Copying '$source_file_path' to '$temp_file_path'"
 		Copy-File $source_file_path $temp_file_path
 		if (Test-Path $destination_file_path)
 		{
@@ -22,7 +22,7 @@ function Deploy-FileTemp {
 		Write-Host "Copying '$temp_file_path' to '$destination_file_path'"
 		if (Test-Path $destination_file_path)
 		{
-			Write-Error "Can not copy file: destination file exists:"
+			Write-Error "Can not copy file: destination file exists"
 			Write-Host "File: '$destination_file_path"
 			Break
 		}
