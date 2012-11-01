28cd647e28c7e4e6991b7d7f4884855cd3dbb89b
Savinykh Alexander <hinstance@yandex.ru>
2012-11-01 15:51:38 +0400

    updated

--- Deploy-File.ps1
+++ Deploy-File.ps1
@@ -1,6 +1,9 @@
-﻿. .\Copy-File.ps1
+﻿$scriptLocation = Split-Path $MyInvocation.MyCommand.Path
+$requireThis = $scriptLocation + "\Copy-File.ps1"
+. $requireThis
 
-function Deploy-FileTemp {
+function Deploy-FileTemp 
+{
 	param([string]$source_file_path, [string]$destination_file_path)
 	if (Test-Path $source_file_path)
 	{
@@ -19,7 +22,6 @@ function Deploy-FileTemp {
 			}
 			Remove-Item $destination_file_path -ea stop
 		}
-		Write-Host "Copying '$temp_file_path' to '$destination_file_path'"
 		if (Test-Path $destination_file_path)
 		{
 			Write-Error "Can not copy file: destination file exists"
@@ -28,6 +30,7 @@ function Deploy-FileTemp {
 		}
 		else
 		{
+			Write-Host "Copying '$temp_file_path' to '$destination_file_path'"
 			trap 
 			{
 				Write-Error "While copying file:"
@@ -36,6 +39,48 @@ function Deploy-FileTemp {
 				Continue
 			}
 			Copy-File $temp_file_path $destination_file_path
+		}
+	}
+	else
+	{
+		Write-Error "Source file '$source_file_path' does not exist"
+	}
+}
+
+function Deploy-File 
+{
+	param([string]$source_file_path, [string]$destination_file_path)
+	if (Test-Path $source_file_path)
+	{
+		if (Test-Path $destination_file_path)
+		{
+			Write-Host "Removing destination file '$destination_file_path'..."
+			trap
+			{
+				Write-Error "Can not remove destination file:"
+				Write-Host "File: '$destination_file_path"
+				Write-Host "Error: '$_'"
+				Continue
+			}
+			Remove-Item $destination_file_path -ea stop
+		}
+		if (Test-Path $destination_file_path)
+		{
+			Write-Error "Can not copy file: destination file exists"
+			Write-Host "File: '$destination_file_path"
+			Break
+		}
+		else
+		{
+			Write-Host "Copying '$source_file_path' to '$destination_file_path'"
+			trap 
+			{
+				Write-Error "While copying file:"
+				Write-Host "Files: '$source_file_path' to '$destination_file_path'"
+				Write-Host "Error: '$_'"
+				Continue
+			}
+			Copy-File $source_file_path $destination_file_path
 		}
 	}
 	else
