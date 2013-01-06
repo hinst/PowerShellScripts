965e311b7d83b06b0bcd98b8c14fc98d7ddd7f20
Alexander Savinoff <hinstance@yandex.ru>
2013-01-06 18:35:32 +0400

    decided to include My7z.ps1 in the repository

---
+++ My7z.ps1
@@ -0,0 +1,12 @@
+﻿. myArchiving.ps1
+$My7z = 'C:\Program Files\7-Zip\7z.exe'
+$DelphiProjectBackupExclusions = @("-xr!.git", "-xr!__history", "-xr!*.exe", "-xr!bugreport.txt", "-xr!*.map", "-xr!*.dcu", "-xr!*.map", "-xr!*.dll", "-xr!*.identcache", "-xr!*.mes", "-xr!*.local", "-xr!*.tvsconfig", "-xr!Compiled", "-xr!Log")
+$VisualStudioProject = @("-xr!.git", "-xr!")
+
+function Archive7z([string]$folder, [string]$options)
+{
+	$date = GetArchiveDate
+	$parameters = @("a", "-t7z", "-m0=lzma2", "-mx=7", """$folder $date.7z""", "$folder\*") + $options
+	Write-Host "Debug: now calling 7z with the following parameters: $My7z $parameters"
+	& $My7z $parameters
+}
