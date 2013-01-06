086a1588888b6e68af6b468cd5e5e4e847c1e589
Alexander Savinoff <hinstance@yandex.ru>
2013-01-06 20:13:50 +0400

    My7z.ps1 updated. 7-zip Ingone options for visual studio projects added

--- My7z.ps1
+++ My7z.ps1
@@ -1,7 +1,9 @@
-﻿. myArchiving.ps1
+﻿$ScriptPath = $MyInvocation.MyCommand.Path
+$ScriptFolderPath = Split-Path $ScriptPath
+. "$ScriptFolderPath\Archiving-Utilities.ps1"
 $My7z = 'C:\Program Files\7-Zip\7z.exe'
 $DelphiProjectBackupExclusions = @("-xr!.git", "-xr!__history", "-xr!*.exe", "-xr!bugreport.txt", "-xr!*.map", "-xr!*.dcu", "-xr!*.map", "-xr!*.dll", "-xr!*.identcache", "-xr!*.mes", "-xr!*.local", "-xr!*.tvsconfig", "-xr!Compiled", "-xr!Log")
-$VisualStudioProject = @("-xr!.git", "-xr!")
+$VisualStudioProject = @("""-xr!.git""", "-xr!obj", "-xr!bin", "-xr!packages")
 
 function Archive7z([string]$folder, [string]$options)
 {
